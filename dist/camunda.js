3948e65652e82f02cd5b67b60ca8e814222bdf2a
Jamen <31097748+Jamen1147@users.noreply.github.com>
2019-09-24 17:27:32 +1000

    added one more camunda notification
    now if the user set an order as 'running', 3 notifications will be shown, 1st is that the order has been set to running, 2nd is that a Camunda task is starting and the last is that a Camunda task has been created.

--- dist/camunda.js
+++ dist/camunda.js
@@ -1,100 +1,101 @@
 'use strict';
 
 System.register(['./utils'], function (_export, _context) {
-  "use strict";
+	"use strict";
 
-  var utils, restructure, post, startQACheck;
-  return {
-    setters: [function (_utils) {
-      utils = _utils;
-    }],
-    execute: function () {
-      restructure = function restructure(p) {
+	var utils, restructure, post, startQACheck;
+	return {
+		setters: [function (_utils) {
+			utils = _utils;
+		}],
+		execute: function () {
+			restructure = function restructure(p) {
+				p.ingredient.applicators.forEach(function (app) {
+					delete app.$$hashKey;
+					app.materials.forEach(function (mat) {
+						delete mat.$$hashKey;
+						delete mat.gramsTotal;
+						delete mat.materialId;
+						delete mat.oz;
+						delete mat.seriseId;
+						mat.formulaWt = mat.gramsOnScale;
+						delete mat.gramsOnScale;
+						mat.actualAvg = null;
+						mat.actualDiff = null;
+						mat.actualWt = null;
+					});
+				});
 
-        p.ingredient.applicators.forEach(function (app) {
-          delete app.$$hashKey;
-          app.materials.forEach(function (mat) {
-            delete mat.$$hashKey;
-            delete mat.gramsTotal;
-            delete mat.materialId;
-            delete mat.oz;
-            delete mat.seriseId;
-            mat.formulaWt = mat.gramsOnScale;
-            delete mat.gramsOnScale;
-            mat.actualAvg = null;
-            mat.actualDiff = null;
-            mat.actualWt = null;
-          });
-        });
+				var check = {
+					productId: p.id,
+					productDescription: p.product_desc,
+					ingredient: {
+						applicators: p.ingredient.applicators
+					},
+					meta: {
+						isManual: false,
+						checkCount: 1,
+						isLastCheck: false,
+						rangeMetrix: {}
+					},
+					conditionOfBelts: 'Good',
+					beltDescription: null
+				};
 
-        var check = {
-          productId: p.id,
-          productDescription: p.product_desc,
-          ingredient: {
-            applicators: p.ingredient.applicators
-          },
-          meta: {
-            isManual: false,
-            checkCount: 1,
-            isLastCheck: false,
-            rangeMetrix: {}
-          },
-          conditionOfBelts: 'Good',
-          beltDescription: null
-        };
+				return check;
+			};
 
-        return check;
-      };
+			post = function post(url, param, json) {
+				return new Promise(function (resolve, reject) {
+					var xhr = new XMLHttpRequest();
+					xhr.open('POST', url + param);
+					xhr.onreadystatechange = handleResponse;
+					xhr.setRequestHeader('Content-Type', 'application/json');
+					xhr.onerror = function (e) {
+						return reject(e);
+					};
+					xhr.send(json);
 
-      post = function post(url, param, json) {
-        return new Promise(function (resolve, reject) {
-          var xhr = new XMLHttpRequest();
-          xhr.open('POST', url + param);
-          xhr.onreadystatechange = handleResponse;
-          xhr.setRequestHeader("Content-Type", "application/json");
-          xhr.onerror = function (e) {
-            return reject(e);
-          };
-          xhr.send(json);
+					function handleResponse() {
+						if (xhr.readyState === 4) {
+							if (xhr.status < 300 && xhr.status >= 200) {
+								resolve(xhr.responseText);
+							} else {
+								reject(xhr.responseText);
+							}
+						}
+					}
+				});
+			};
 
-          function handleResponse() {
-            if (xhr.readyState === 4) {
-              if (xhr.status < 300 && xhr.status >= 200) {
-                resolve(xhr.responseText);
-              } else {
-                reject(xhr.responseText);
-              }
-            }
-          }
-        });
-      };
+			_export('startQACheck', startQACheck = function startQACheck(product, line) {
+				var FORM_KEY = 'QAFormProductOnly';
+				var PATH = 'process-definition/key/' + FORM_KEY + '/start';
 
-      _export('startQACheck', startQACheck = function startQACheck(product, line) {
-        var FORM_KEY = 'QAFormProductOnly';
-        var PATH = 'process-definition/key/' + FORM_KEY + '/start';
+				var p = restructure(product);
 
-        var p = restructure(product);
+				var toSend = {
+					variables: {
+						_currentLine: { value: line, type: 'String' },
+						_currentCheck: { value: 1, type: 'Long' },
+						_lastCheck: { value: false, type: 'Boolean' },
+						_product: { value: JSON.stringify(p), type: 'json' },
+						_allChecks: { value: '[]', type: 'json' }
+					},
+					businessKey: null
+				};
 
-        var toSend = {
-          variables: {
-            _currentLine: { value: line, type: "String" },
-            _currentCheck: { value: 1, type: "Long" },
-            _lastCheck: { value: false, type: "Boolean" },
-            _product: { value: JSON.stringify(p), type: "json" },
-            _allChecks: { value: "[]", type: "json" }
-          },
-          businessKey: null
-        };
+				utils.alert('success', 'Starting...', 'A Camunda QA Check Process is starting...');
 
-        post(utils.camundaRestApi, PATH, JSON.stringify(toSend)).then(function (res) {
-          utils.alert('success', 'Successful', 'A Camunda QA Check Process has been started');
-        }).catch(function (e) {
-          utils.alert('error', 'Connection Error', 'Camunda QA Check Process failed to start due to ' + e + ' but you can still start it manually');
-        });
-      });
+				post(utils.camundaRestApi, PATH, JSON.stringify(toSend)).then(function (res) {
+					utils.alert('success', 'Successful', 'A Camunda QA Check Process has been started');
+				}).catch(function (e) {
+					utils.alert('error', 'Connection Error', 'Camunda QA Check Process failed to start due to ' + e + ' but you can still start it manually');
+				});
+			});
 
-      _export('startQACheck', startQACheck);
-    }
-  };
+			_export('startQACheck', startQACheck);
+		}
+	};
 });
 //# sourceMappingURL=camunda.js.map
--- src/camunda.js
+++ src/camunda.js
@@ -1,88 +1,95 @@
-import * as utils from './utils'
+import * as utils from './utils';
 
 /**
  * This function restructures the product object into a check object for better manipulations in camunda qa check
  * @param {Object} p Product Object
  * @return {Object} restructured product object
  */
-const restructure = p => {
+const restructure = (p) => {
+	p.ingredient.applicators.forEach((app) => {
+		delete app.$$hashKey;
+		app.materials.forEach((mat) => {
+			delete mat.$$hashKey;
+			delete mat.gramsTotal;
+			delete mat.materialId;
+			delete mat.oz;
+			delete mat.seriseId;
+			mat.formulaWt = mat.gramsOnScale;
+			delete mat.gramsOnScale;
+			mat.actualAvg = null;
+			mat.actualDiff = null;
+			mat.actualWt = null;
+		});
+	});
 
-  p.ingredient.applicators.forEach( app => {
-    delete app.$$hashKey
-    app.materials.forEach( mat => {
-      delete mat.$$hashKey
-      delete mat.gramsTotal
-      delete mat.materialId
-      delete mat.oz
-      delete mat.seriseId
-      mat.formulaWt = mat.gramsOnScale
-      delete mat.gramsOnScale
-      mat.actualAvg = null
-      mat.actualDiff = null
-      mat.actualWt = null
-    })
-  })
+	const check = {
+		productId: p.id,
+		productDescription: p.product_desc,
+		ingredient: {
+			applicators: p.ingredient.applicators
+		},
+		meta: {
+			isManual: false,
+			checkCount: 1,
+			isLastCheck: false,
+			rangeMetrix: {}
+		},
+		conditionOfBelts: 'Good',
+		beltDescription: null
+	};
 
-  const check = {
-    productId: p.id,
-    productDescription: p.product_desc,
-    ingredient: {
-      applicators: p.ingredient.applicators
-    },
-    meta: {
-      isManual: false,
-      checkCount: 1,
-      isLastCheck: false,
-      rangeMetrix: {}
-    },
-    conditionOfBelts: 'Good',
-    beltDescription: null
-  }
-
-  return check
-}
+	return check;
+};
 
 const post = (url, param, json) => {
-  return new Promise((resolve, reject) => {
-    var xhr = new XMLHttpRequest()
-    xhr.open('POST', url + param)
-    xhr.onreadystatechange = handleResponse
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.onerror = e => reject(e)
-    xhr.send(json)
+	return new Promise((resolve, reject) => {
+		var xhr = new XMLHttpRequest();
+		xhr.open('POST', url + param);
+		xhr.onreadystatechange = handleResponse;
+		xhr.setRequestHeader('Content-Type', 'application/json');
+		xhr.onerror = (e) => reject(e);
+		xhr.send(json);
 
-    function handleResponse () {
-      if (xhr.readyState === 4) {
-        if (xhr.status < 300 && xhr.status >= 200) {
-          resolve(xhr.responseText)
-        } else {
-          reject(xhr.responseText)
-        }
-      }
-    }
-  })
-}
+		function handleResponse() {
+			if (xhr.readyState === 4) {
+				if (xhr.status < 300 && xhr.status >= 200) {
+					resolve(xhr.responseText);
+				} else {
+					reject(xhr.responseText);
+				}
+			}
+		}
+	});
+};
 
 export const startQACheck = (product, line) => {
-  const FORM_KEY = 'QAFormProductOnly'
-  const PATH = `process-definition/key/${FORM_KEY}/start`
-  
-  const p = restructure(product)
+	const FORM_KEY = 'QAFormProductOnly';
+	const PATH = `process-definition/key/${FORM_KEY}/start`;
 
-  const toSend = {
-    variables : {
-      _currentLine : { value : line, type : "String" },
-      _currentCheck: { value : 1, type : "Long" },
-      _lastCheck: { value : false, type : "Boolean" },
-      _product: { value : JSON.stringify(p), type : "json" },
-      _allChecks: { value : "[]", type : "json" }
-    }, 
-    businessKey : null
-  }
+	const p = restructure(product);
 
-  post(utils.camundaRestApi, PATH, JSON.stringify(toSend)).then(res => {
-    utils.alert('success', 'Successful', 'A Camunda QA Check Process has been started')
-  }).catch(e => {
-    utils.alert('error', 'Connection Error', `Camunda QA Check Process failed to start due to ${e} but you can still start it manually`)
-  })
-}
+	const toSend = {
+		variables: {
+			_currentLine: { value: line, type: 'String' },
+			_currentCheck: { value: 1, type: 'Long' },
+			_lastCheck: { value: false, type: 'Boolean' },
+			_product: { value: JSON.stringify(p), type: 'json' },
+			_allChecks: { value: '[]', type: 'json' }
+		},
+		businessKey: null
+	};
+
+	utils.alert('success', 'Starting...', 'A Camunda QA Check Process is starting...');
+
+	post(utils.camundaRestApi, PATH, JSON.stringify(toSend))
+		.then((res) => {
+			utils.alert('success', 'Successful', 'A Camunda QA Check Process has been started');
+		})
+		.catch((e) => {
+			utils.alert(
+				'error',
+				'Connection Error',
+				`Camunda QA Check Process failed to start due to ${e} but you can still start it manually`
+			);
+		});
+};
